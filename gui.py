5f73046e99fdeb736ddf29d10a32cb1929a2ea88
下田直樹 <naoki@shimodanaokinoMac-Studio.local>
2024-04-03 18:42:26 +0900

    apr 3 at lab

--- gui.py
+++ gui.py
@@ -13,6 +13,9 @@ class App(ctk.CTk):
 
         # opencv
         self.vcap = cv2.VideoCapture(0)
+        self.vwidth = self.vcap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.vheight = self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        print(self.vwidth, self.vheight)
 
         # create GUI
         self.create_frames()
@@ -22,34 +25,31 @@ class App(ctk.CTk):
 
     def create_frames(self):
         self.side_frame = ctk.CTkFrame(self, fg_color="gray")
-        self.cam_frame = ctk.CTkFrame(self, fg_color="cyan")
+        self.cam_frame = ctk.CTkFrame(self, width=self.vwidth, height=self.vheight, fg_color="cyan")
 
         self.side_frame.pack(side="left", fill="y")
-        self.cam_frame.pack(side="left")
+        self.cam_frame.pack(side="left", expand=True, fill="both")
 
     def create_widgets(self):
         # side bar
         label = ctk.CTkLabel(self.side_frame, text="Hello world.")
         label.pack()
-        button = ctk.CTkButton(self.side_frame, text="Register", command=self.sample_cb)
+        button = ctk.CTkButton(self.side_frame, text="Register")
         button.pack()
 
         # right frame
-        self.canvas1 = ctk.CTkCanvas(self.cam_frame)
-        self.canvas1.grid()
-
-    def sample_cb(self):
-        print(self.winfo_geometry())
+        self.canvas = ctk.CTkCanvas(self.cam_frame)
+        self.canvas.pack(expand=True, fill="both")
 
     def update(self):
         # Get a frame from the video source
         _, frame = self.vcap.read()
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+        self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
 
-        #self.photo -> Canvas
-        self.canvas1.create_image(0,0, image= self.photo, anchor = ctk.NW)
+        # self.photo -> Canvas
+        self.canvas.create_image(0, 0, image=self.photo, anchor=ctk.NW)
 
         self.after(self.delay, self.update)
 
