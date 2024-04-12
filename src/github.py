9b8514f15c76f5688765a5134fe51d3a1483d010
N-Shimoda <naoki.shimoda@outlook.jp>
2024-04-12 22:20:23 +0900

    implemented version check

--- src/github.py
+++ src/github.py
@@ -19,11 +19,13 @@ def get_latest_tag(repo_owner: str, repo_name: str) -> str | None:
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
     response = requests.get(api_url)
     if response.status_code == 200:
-        latest_tag_name = response.json()["tag_name"]
-        return latest_tag_name
+        data = response.json()
+        latest_tag_name = data["tag_name"]
+        release_date = str(data["published_at"]).split("T")[0]
+        return latest_tag_name, release_date
     else:
-        print(f"Failed to fetch latest tag. Status code: {response.status_code}")
-        return None
+        print(f"Failed to fetch latest release info. Status code: {response.status_code}")
+        return None, None
 
 if __name__ == "__main__":
     
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     repo_name = "notion-book-stock"
     VERSION = "v1.1"
 
-    latest_tag = get_latest_tag(repo_owner, repo_name)
+    latest_tag, release_date = get_latest_tag(repo_owner, repo_name)
     if latest_tag:
-        print("Latest tag:", latest_tag)
+        print("Latest tag: '{}' (released on {})".format(latest_tag, release_date))
         print(latest_tag==VERSION)
