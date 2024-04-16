0d1d270e9b02ce7ed0203184be901b8b55da8a33
KU-onji <mgr_127@yahoo.co.jp>
2024-04-16 10:22:05 +0900

    modify initial self.history

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ from pyzbar.pyzbar import decode
 
 from src.github import get_latest_tag
 from src.google_books import search_isbn
-from src.notion import add_book_info
+from src.notion import add_book_info, get_isbn_list
 
 # modify these values when creating new release
 VERSION = "v1.2"
@@ -34,7 +34,7 @@ class App(ctk.CTk):
         self.geometry("1024x640")
 
         # --- variables ---
-        self.history = []
+        self.history = get_isbn_list()
         self.cmbbox = None
 
         try:
